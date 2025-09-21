cbfdea448a6aa3bc2ad45433e9796443f12d17e7
Elleni-Bisrat <ellenibisrat16@gmail.com>
2025-09-21 21:40:43 +0300

    update

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,72 +1,48 @@
-interface DirectorInterface {
+
+export interface DirectorInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
   workDirectorTasks(): string;
 }
 
-interface TeacherInterface {
+export interface TeacherInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
   workTeacherTasks(): string;
 }
 
-class Director implements DirectorInterface {
+export class Director implements DirectorInterface {
   workFromHome(): string {
     return "Working from home";
   }
-
   getCoffeeBreak(): string {
     return "Getting a coffee break";
   }
-
   workDirectorTasks(): string {
     return "Getting to director tasks";
   }
 }
 
-class Teacher implements TeacherInterface {
+export class Teacher implements TeacherInterface {
   workFromHome(): string {
     return "Cannot work from home";
   }
-
   getCoffeeBreak(): string {
     return "Cannot have a break";
   }
-
   workTeacherTasks(): string {
     return "Getting to work";
   }
 }
 
-function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === "number") {
-    if (salary < 500) {
-      return new Teacher();
-    } else {
-      return new Director();
-    }
-  } else if (typeof salary === "string") {
-    return new Director();
-  }
-  return new Teacher();
-}
-
-
-function isDirector(employee: Director | Teacher): employee is Director {
+export function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
   return (employee as Director).workDirectorTasks !== undefined;
 }
 
-function executeWork(employee: Director | Teacher): string {
+export function executeWork(employee: DirectorInterface | TeacherInterface): string {
   if (isDirector(employee)) {
     return employee.workDirectorTasks();
   } else {
     return employee.workTeacherTasks();
   }
-}
-
-type Subjects='Math'|'History';
-
-function teachClass(todayClass:Subjects):string{
-  if(todayClass==='Math') return 'Teaching Math';
-  return 'Teaching History';
 }
